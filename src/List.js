fe7d9a959c642d89e68c14995d5b8026aa1c9e00
fabioab1 <fabiobonanatodeabreu990@gmail.com>
2025-05-25 23:24:52 -0300

    feat: adiciona o componente List do tutorial implementando

--- src/List.js
+++ src/List.js
@@ -1,11 +1,33 @@
 import { Link } from "react-router-dom";
 import { useState } from "react";
 
+let nextId = 0;
+
 export default function List() {
+    const [name, setName] = useState('');
+    const [artists, setArtists] = useState([]);
+    
     return (
         <>
+            <h1>Inspirando escultores:</h1>
+            <input
+                value={name}
+                onChange={e => setName(e.target.value)}
+            />
+            <button onClick={() => {
+                setArtists(
+                    [
+                        ...artists,
+                        { id: nextId++, name: name }
+                    ]
+                );
+            }}>Adicionar</button>
+            <ul>
+                {artists.map(artist => (
+                    <li key={artist.id}>{artist.name}</li>
+                ))}
+            </ul>
 
-        
             <Link to="/Atv04">Voltar</Link>
         </>
     );
